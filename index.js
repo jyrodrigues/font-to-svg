1dff129e948a597b087c7d0b33376f877b086bf2
José Rodrigues <jyrodrigues@gmail.com>
2020-09-23 16:48:52 -0300

    Finish saving font as elm module file

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ const svgParser = require('svg-parser');
 const util = require('util');
 const svgpath = require('svgpath');
 
-fs.readFile('./Barlow/Barlow-Light.ttf', function (err, buffer) {
-    if (!!err) throw err;
-
+const processFontBuffer = (buffer) => {
+    // Transform True Type Font into SVG
+    // https://github.com/qdsang/ttf2svg
     const svgContent = ttf2svg(buffer);
 
     // fs.writeFileSync('./Barlow-Light.svg', svgContent);
 
+    // Parse SVG string into HAST (Hypertext Abstract Syntax Tree format)
+    // https://github.com/Rich-Harris/svg-parser
     const root = svgParser.parse(svgContent);
 
-    // console.log(util.inspect(parsed, false, null, true));
-    // fs.writeFileSync('./Barlow-Light.json', JSON.stringify(parsed));
+    // console.log(util.inspect(root, false, null, true));
+    // fs.writeFileSync('./Barlow-Light.json', JSON.stringify(root));
 
+    // Extract Glyphs and maximum advancement after glyph drawing for re-scaling
     let svg = root.children[0];
     let defs = svg.children[1];
     let barlowLightFont = defs.children[0];
@@ -26,9 +29,11 @@ fs.readFile('./Barlow/Barlow-Light.ttf', function (err, buffer) {
     );
     let glyphs = barlowLightFont.children.filter(elem => elem.tagName === "glyph");
 
+    let formattedGlyphs = {};
+
+    // Transform each Glyph path
     for (glyph of glyphs) {
         let char = String.fromCharCode(parseInt(glyph.properties.unicode.replace("&#x", ""),16))
-        if (!"Welcome".includes(char)) { continue }
 
         let rawPath = glyph.properties.d;
         let pathStr = svgpath(rawPath)
@@ -38,8 +43,11 @@ fs.readFile('./Barlow/Barlow-Light.ttf', function (err, buffer) {
             .rel()
             .toString();
 
+        // Transform to string and back because `svgpath` stacks operations like `.rel()`
+        // and we want to manually insert svg moves to reset the relative position.
         let path = svgpath(pathStr);
 
+        // Stack svg moves to reset them at the end of the path in reverse order (could be any order).
         let movesToUndo = [];
         for (let segment of path.segments) {
             if (segment[0] === 'M') { segment[0] = 'm'; }
@@ -53,16 +61,81 @@ fs.readFile('./Barlow/Barlow-Light.ttf', function (err, buffer) {
             move[2] = -move[2];
             path.segments.push(move);
         }
+
+        // Add a final svg move by how much the font defines this glyph to "advance"
         path.segments.push(
             [ 'm', (glyph.properties['horiz-adv-x'] / advanceRatio) * 10, 0 ],
             // [ 'l', 10, 0 ]
         )
 
+        let formattedPathSegments = path.round(6).segments;
+
+        formattedGlyphs[char] = formattedPathSegments;
+
         // console.log(char);
         // console.log(glyph.properties);
         // console.log(util.inspect(path, false, null, true));
-        console.log(path.round(6).toString());
+        // console.log(formattedPathString);
     }
 
+    return formattedGlyphs;
+}
 
+// `moduleName` should be in PascalCase
+const fontToElmModule = (moduleName, glyphsObject) => {
+    let elmCode = `
+module Svg.${moduleName} exposing (lettersDict)
+
+import Dict exposing (Dict)
+import Svg.Core exposing (PathSegment(..))
+
+
+lettersDict : Dict Char (List PathSegment)
+lettersDict =
+    Dict.fromList`;
+
+    let count = 0;
+
+    for (let glyph in glyphsObject) {
+        if (glyph !== '0') { continue; }
+        if (count === 0) {
+            elmCode += `\n        [ ( '${glyph}'`;
+        } else {
+            elmCode += `\n        , ( '${glyph}'`;
+        }
+        count += 1;
+
+        let segments = glyphsObject[glyph];
+
+        for (let i = 0; i < segments.length; i++) {
+            let seg = segments[i];
+
+            elmCode += `\n        ${(i === 0 ? "  , [" : "    ,")} ${seg[0].toUpperCase()}`;
+
+            if (seg[1] !== undefined) {
+                elmCode += ` ( ${seg[1]}, ${seg[2]} )`
+            }
+
+            if (seg[3] !== undefined) {
+                elmCode += ` ( ${seg[3]}, ${seg[4]} )`
+            }
+        }
+
+        elmCode += "\n            ]";
+        elmCode += "\n          )";
+    }
+
+    elmCode += "\n        ]";
+
+    return elmCode;
+}
+
+fs.readFile('./Barlow/Barlow-Light.ttf', (err, buffer) => {
+    if (!!err) throw err;
+
+    let formattedGlyphs = processFontBuffer(buffer);
+    let moduleName = "FontBarlowLight"
+    let elmFileAsString = fontToElmModule(moduleName, formattedGlyphs);
+
+    fs.writeFileSync(`${moduleName}.elm`, elmFileAsString);
 });
